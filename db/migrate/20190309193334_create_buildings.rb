64c14e1f54d7dc1e18ba66e4e4197684bd786d4d
Gabriel Kutsovsky <gkutsovsky@gmail.com>
2019-03-09 15:22:12 -0500

    added has_many

--- db/migrate/20190309193334_create_buildings.rb
+++ db/migrate/20190309193334_create_buildings.rb
@@ -1,4 +1,8 @@
 class CreateBuildings < ActiveRecord::Migration[5.2]
+  has_many :users
+  has_many :shareables
+  has_many :transactions
+  
   def change
     create_table :buildings do |t|
       t.string :building_name
