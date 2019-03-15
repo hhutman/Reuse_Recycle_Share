d2eeed59204c0ad6add4692e526e158dc39c89ce
Kevin Choi <kevinyoungchoi@gmail.com>
2019-03-15 09:55:20 -0400

    ready to pull

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController 
   def index 
+    if current_user 
+      redirect_to building_path(current_user.buildings.first)
+    end 
+    else
+      redirect_to '/users/sign_in'
+    end  
   end
 end 
