f296fea63d364d32b856922f5a808642451927ae
Kevin Choi <kevinyoungchoi@gmail.com>
2019-03-19 22:57:34 -0400

    replaced owner id to user id

--- db/migrate/20190409195336_add_cols_to_goods_table.rb
+++ db/migrate/20190409195336_add_cols_to_goods_table.rb
@@ -1,6 +1,6 @@
 class AddColsToGoodsTable < ActiveRecord::Migration[5.2]
   def change
     add_column :goods, :title, :string
-    add_column :goods, :owner_id, :integer 
+    add_column :goods, :user_id, :integer 
   end
 end
--- db/migrate/20190409195337_add_cols_to_service.rb
+++ db/migrate/20190409195337_add_cols_to_service.rb
@@ -1,6 +1,6 @@
 class AddColsToService < ActiveRecord::Migration[5.2]
   def change
     add_column :services, :title, :string
-    add_column :services, :owner_id, :integer 
+    add_column :services, :user_id, :integer 
   end
 end
